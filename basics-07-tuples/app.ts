dace177535821989c5a720dde75ac09d8bbf9e62
ycw0519 <ycw0519@naver.com>
2024-09-05 10:15:05 +0900

    [add] TypeScript 기본&기본 타입 학습 완료

---
+++ basics-07-tuples/app.ts
@@ -0,0 +1,20 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: "Maximilian",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
+};
+
+// person.role.push("admin");
+// person.role[1] = 10;
+
+// person.role = [0, "admin", "user"];
