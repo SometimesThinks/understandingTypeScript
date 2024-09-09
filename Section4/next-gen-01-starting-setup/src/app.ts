d12203f8145f44eb8985e9b29c129448384725c1
ycw0519 <ycw0519@naver.com>
2024-09-09 17:42:58 +0900

    [add] 차세대 JS와 TS 학습 완료

---
+++ Section4/next-gen-01-starting-setup/src/app.ts
@@ -0,0 +1,18 @@
+const userName = "Max";
+let age = 30;
+
+age = 29;
+
+function add(a: number, b: number) {
+  var result;
+  result = a + b;
+  return result;
+}
+
+// if (age > 20) {
+//   let isOld = true;
+// }
+
+// console.log(isOld);
+
+// console.log(result);
